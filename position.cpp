43788fee20ac815eb84e415c45e7cf94e5117b09
wchorolque <wchorolque@gmail.com>
2012-12-27 01:22:34 -0400

    Agregated the Position Class and corrected model

--- position.cpp
+++ position.cpp
@@ -1,5 +1,16 @@
 #include "position.h"
 
-Position::Position()
+Position::Position(QString name, QString description)
 {
+    this->m_Name = name;
+    this->m_Description = description;
+}
+
+QString Position::toString()
+{
+    QString str = QString("Name: %1 Description: %2\n")
+            .arg(this->m_Name)
+            .arg(this->m_Description);
+
+    return str;
 }
--- position.h
+++ position.h
@@ -1,10 +1,16 @@
 #ifndef POSITION_H
 #define POSITION_H
 
+#include <QString>
+
 class Position
 {
+private:
+    QString m_Name;
+    QString m_Description;
 public:
-    Position();
+    Position(QString name, QString description);
+    QString toString();
 };
 
 #endif // POSITION_H
